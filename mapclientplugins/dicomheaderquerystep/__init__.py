ffc6bdfda80095316a5528b6287bf933d2d90202
Hugh Sorby <h.sorby@auckland.ac.nz>
2015-05-18 16:08:25 +1200

    Initial commit of skeleton step.

---
+++ mapclientplugins/dicomheaderquerystep/__init__.py
@@ -0,0 +1,13 @@
+
+'''
+MAP Client Plugin
+'''
+
+__version__ = '0.1.0'
+__author__ = 'Hugh Sorby'
+__stepname__ = 'Dicom Header Query'
+__location__ = 'https://github.com/mapclient-plugins/dicomheaderquery'
+
+# import class that derives itself from the step mountpoint.
+from mapclientplugins.dicomheaderquerystep import step
+
